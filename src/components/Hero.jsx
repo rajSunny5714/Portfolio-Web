611bca5053c3148ed9060d21cb147a0294aed5fa
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2026-02-12 19:16:55 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
             initial="hidden"
             animate="visible"
             className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-              Sunny Raj Jha
+              Sunny Raj
             </motion.h1>
             <motion.span 
              variants={container(0.5)}
@@ -68,4 +68,5 @@ const Hero = () => {
 
 
 export default Hero;
+
 
